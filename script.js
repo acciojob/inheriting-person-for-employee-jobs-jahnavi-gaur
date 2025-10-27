2b5db4e8a8ed10836ea6a64f964e1e4c853f9144
jahnavi-gaur <121301783+jahnavi-gaur@users.noreply.github.com>
2025-10-27 10:39:35 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,15 +1,13 @@
-// complete this js code
 function Person(name, age) {
   this.name = name;
   this.age = age;
 }
 
 Person.prototype.greet = function() {
-  console.log(`Hello, my name is ${this.name}, I am ${this.age} years old.`);
+  console.log(`Hello, my name is ${this.name} and I am ${this.age} years old.`);
 };
 
 function Employee(name, age, jobTitle) {
-  // Call the Person constructor to initialize name and age
   Person.call(this, name, age);
   this.jobTitle = jobTitle;
 }
